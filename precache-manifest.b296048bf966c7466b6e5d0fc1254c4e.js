8376680e22407ab30ffd76a9e4b29445e6be2af3
Kichan Yoon <pro4697@naver.com>
2020-07-12 23:07:28 +0900

    Updates

---
+++ precache-manifest.b296048bf966c7466b6e5d0fc1254c4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afec900b088163da0d12df7ce2177a97",
+    "url": "/netflix_app/index.html"
+  },
+  {
+    "revision": "ba0387ea998e217f0275",
+    "url": "/netflix_app/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "32922825f5433e8a492e",
+    "url": "/netflix_app/static/js/2.53f6f328.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix_app/static/js/2.53f6f328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0387ea998e217f0275",
+    "url": "/netflix_app/static/js/main.6e5ed672.chunk.js"
+  },
+  {
+    "revision": "e6d7a0c27e50f2459149",
+    "url": "/netflix_app/static/js/runtime-main.7f76722e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/netflix_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
