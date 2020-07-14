662e2adcc8f68d889a9a5b1f71a842587ad77fcc
Kichan Yoon <pro4697@naver.com>
2020-07-15 00:40:24 +0900

    Detail UI fix

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -19,17 +19,26 @@ class Detail extends React.Component {
             style={{ backgroundImage: `url(${location.state.backdrop})` }}
           />
           <br />
-          <div
-            className='detail__poster'
-            style={{ backgroundImage: `url(${location.state.poster})` }}
-          />
-          <div className='detail__info'>
-            <div className='detail__title'>{location.state.title}</div>
-            <div className='detail__title'>{location.state.date}</div>
+          <div className='detail__top-box'>
+            <div
+              className='detail__poster'
+              style={{ backgroundImage: `url(${location.state.poster})` }}
+            />
+            <div className='detail__info'>
+              <div className='detail__title'>{location.state.title}</div>
+              <div className='detail__title sub'>{`${location.state.date[0]}. ${location.state.date[1]}. ${location.state.date[2]}`}</div>
+              <div className='detail__description'>
+                <div className='detail__overview'>
+                  {location.state.overview}
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='detail__description'>
-            <div className='detail__overview'>{location.state.overview}</div>
-          </div>
+          {/* <div className='detail__bottom-box'>
+            <div className='detail__description'>
+              <div className='detail__overview'>{location.state.overview}</div>
+            </div>
+          </div> */}
         </div>
       );
     } else {
