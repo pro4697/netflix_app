a723b504bc0e250d4783a22627ac3556ebecdb85
Kichan Yoon <pro4697@naver.com>
2020-07-17 00:16:41 +0900

    Route Transition add
    페이지전환 애니메이션 추가.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
+import {
+  HashRouter,
+  Switch,
+  Route,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import About from './routes/About';
 import Home from './routes/Home';
 import Navigation from './components/Navigation';
@@ -10,9 +16,25 @@ function App() {
   return (
     <HashRouter>
       <Navigation />
-      <Route path='/' exact={true} component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/movie-detail' component={Detail} />
+      <Route
+        render={({ location }) => {
+          return (
+            <TransitionGroup>
+              <CSSTransition
+                key={location.pathname}
+                timeout={220}
+                classNames='fade'
+              >
+                <Switch location={location}>
+                  <Route exact path='/' component={Home} />
+                  <Route path='/about' component={About} />
+                  <Route path='/movie-detail' component={Detail} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          );
+        }}
+      ></Route>
     </HashRouter>
   );
 }
