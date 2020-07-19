a90bc5ee8f953266c05918eeafb89a805bfe9a51
Kichan Yoon <pro4697@naver.com>
2020-07-20 00:08:33 +0900

    Updates

--- precache-manifest.4128c1ac079922598727458bfeec9f29.js
+++ precache-manifest.4128c1ac079922598727458bfeec9f29.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ffda2fcbe6b409e1abb86abbc073971e",
+    "revision": "2b46cbff7436a96ad3d171aea51d371c",
     "url": "/netflix_app/index.html"
   },
   {
-    "revision": "c0c1fce141303b76275e",
-    "url": "/netflix_app/static/css/main.c1328268.chunk.css"
+    "revision": "6315ad4628ced3ae71b1",
+    "url": "/netflix_app/static/css/main.9668ca37.chunk.css"
   },
   {
     "revision": "22409d2346f2c23b44d1",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/netflix_app/static/js/2.a851632a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c0c1fce141303b76275e",
-    "url": "/netflix_app/static/js/main.92abca05.chunk.js"
+    "revision": "6315ad4628ced3ae71b1",
+    "url": "/netflix_app/static/js/main.ed690384.chunk.js"
   },
   {
     "revision": "e6d7a0c27e50f2459149",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflix_app/precache-manifest.86ee8b3d3004887e3aa02e0e5e128fd6.js"
+  "/netflix_app/precache-manifest.4128c1ac079922598727458bfeec9f29.js"
 );
 
 self.addEventListener('message', (event) => {
