ba5f160b64124ff652a7103a39671e69027ef5db
Kichan Yoon <pro4697@naver.com>
2020-09-03 19:51:49 +0900

    class형 컴포넌트로 일관되게 변경

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Flicking from '@egjs/react-flicking';
+import { Fade, AutoPlay } from '@egjs/flicking-plugins';
+import { motion } from 'framer-motion';
+import Section from '../components/Section';
+import NavBottom from '../components/NavBottom';
+import Loader from '../components/Loader';
+import './Home.css';
 import { utils } from '../utils';
 import { movieApi } from '../Api';
-import Home from './HomeRender';
 
-export default () => {
-  const [movies, setMovies] = useState({
+const plugins = [new Fade(), new AutoPlay(2600, 'NEXT')];
+
+class Home extends React.Component {
+  state = {
     isLoading: true,
     nowPlaying: [],
     topRated: [],
     popular: [],
     upComing: [],
-  });
+  };
 
-  const getData = async () => {
+  async componentDidMount() {
     const {
       data: { results: nowPlaying },
     } = await movieApi.nowPlaying();
 
+    // 한글로 번역된 줄거리가 없는 경우 영문 줄거리로 대체
     const {
       data: { results: overview1 },
     } = await movieApi.nowPlayingUS();
@@ -49,19 +58,57 @@ export default () => {
     } = await movieApi.upComingUS();
     utils.overview_replace(upComing, overview4);
 
-    setMovies({
+    this.setState({
       isLoading: false,
       nowPlaying,
       topRated,
       popular,
       upComing,
     });
-  };
+  }
 
-  useEffect(() => {
-    getData();
-    console.log('get Data');
-  }, []);
+  render() {
+    const { isLoading, nowPlaying, topRated, popular, upComing } = this.state;
+    return (
+      <>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <motion.div
+            className='container'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {nowPlaying && nowPlaying.length > 0 && (
+              <Flicking
+                className='flicking'
+                circular={true}
+                zIndex={0}
+                duration={400}
+                collectStatistics={false}
+                plugins={plugins}
+              >
+                {utils.Panel_render(nowPlaying)}
+              </Flicking>
+            )}
+            {popular && popular.length > 0 && (
+              <Section title='Popular'>{utils.Movie_render(popular)}</Section>
+            )}
+            {topRated && topRated.length > 0 && (
+              <Section title='Top Rated'>
+                {utils.Movie_render(topRated)}
+              </Section>
+            )}
+            {upComing && upComing.length > 0 && (
+              <Section title='UpComing'>{utils.Movie_render(upComing)}</Section>
+            )}
+          </motion.div>
+        )}
+        <NavBottom props={['white', null, null]} />
+      </>
+    );
+  }
+}
 
-  return <Home {...movies} />;
-};
+export default Home;
